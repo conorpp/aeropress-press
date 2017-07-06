bd1ed2aa5e6b9c23ace9da750202608f5ce598a1
Conor Patrick <conorpp@vt.edu>
2017-07-06 00:48:45 -0400

    add sensor plate.  and mug for show

--- autopress.py
+++ autopress.py
@@ -4,7 +4,7 @@ use('lib/springs.scad')
 
 RESOLUTION = 40
 
-aeropress_d = 15.
+aeropress_d = 20.
 spring_base_x = aeropress_d * 2.
 spring_base_y = aeropress_d * 1.3
 spring_base_z = 5.
@@ -13,26 +13,26 @@ spring_top_z = spring_base_z/2.
 
 lswitch_x = 2.0
 lswitch_y = 1.0
-lswitch_z = 2.2
+lswitch_z = 2.
 lswitch_sink = 0.5
 
 lead_screw_d = 3.0
-lead_screw_h = 24.
+lead_screw_h = 49.5
 lead_screw_pad = 1.0
 
 hexnut_d = 5.
 hexnut_sink = 2.
 
-spring_d = 3.2
+spring_d = 5.2
 spring_h = 5.0
 spring_sink = spring_h/5.
 
 rod_hole_start_h = 1.
-rod_d = 1.0
+rod_d = 2.5
 bearing_od = 2.0
 bearing_id = 1.0
 bearing_h = 3.0
-rod_h = 25.
+rod_h = 51.
 
 stepper_x = 8.
 stepper_y = 8.
@@ -46,7 +46,8 @@ lead_screw_holes = []
 
 pad = spring_d
 
-SPRINGS = 0
+SPRINGS = 1
+SPLICE = 0
 
 def spring_base():
     r = cube([spring_base_x,spring_base_y,spring_base_z])
@@ -83,16 +84,39 @@ def spring_top():
 
 def top_plate():
     r = cube([spring_base_x,spring_base_y,spring_top_z])
+
+    leg_x = pad * 2
+    leg_y = spring_base_y
+    leg_z = spring_top_z*1.4
+    legr = translate([0,0,0])( cube([leg_x, leg_y, leg_z]))
+    legl = translate([spring_base_x - leg_x,0,0])( cube([leg_x, leg_y, leg_z]))
+
     for x in [0+pad, spring_base_x-pad]:
         lsh = translate([x, spring_base_y/2., spring_top_z - hexnut_sink + 1e-3])(cylinder(d = hexnut_d, h = hexnut_sink,segments=6))
         r -= lsh
-        lsh = translate([x, spring_base_y/2., -1e-3])(cylinder(d = lead_screw_d, h = spring_top_z + 1e-3))
+        legr -= lsh
+        legl -= lsh
+        lsh = translate([x, spring_base_y/2., -1e-3])(cylinder(d = lead_screw_d, h = spring_top_z*2))
         r -= lsh
-    
+        legr -= lsh
+        legl -= lsh
+
     for i in spring_locs:
-        r -= translate([i[0],i[1],-1e-3])(cylinder(d = bearing_od, h = spring_top_z + 2e-3))
+        s = translate([i[0],i[1],-1e-3])(cylinder(d = bearing_od, h = spring_top_z*2 + 2e-3))
+        r -= s
+        legr -= s
+        legl -= s
+
+    r -= translate([(spring_base_x - lswitch_x)/2.,(spring_base_y - lswitch_y)/2.,-1e-3])( cube([lswitch_x, lswitch_y, lswitch_sink]) )
+    r += translate([0,0,-leg_z])(legr)
+    r += translate([0,0,-leg_z])(legl)
+    
+    r -= translate([pad*1.5,pad*1.5/4,-spring_top_z])(cube([spring_base_x-2*pad*1.5,spring_base_y,spring_top_z]))
 
     return r
+
+def sense_plate():
+    return translate([pad*1.5,0,0])(cube([spring_base_x-2*pad*1.5,spring_base_y - pad * 1.5/4,spring_top_z/2]))
 
 def roof_plate():
     r = cube([spring_base_x,spring_base_y,spring_top_z])
@@ -103,7 +127,7 @@ def roof_plate():
 
 
 def _spring():
-    return spring(_Windings = 10, R = spring_d/2, r = spring_d/20, h = spring_h, slices = 20)
+    return spring(_Windings = 6, R = spring_d/2, r = spring_d/20, h = spring_h, slices = 20)
 
 def bearing():
     return cylinder(d = bearing_od, h = bearing_h) - translate([0,0,-1e-3])(cylinder(d = bearing_id, h = bearing_h + 2e-3))
@@ -129,14 +153,17 @@ def assembly():
     if SPRINGS:
         for l in spring_locs:
             sb += translate(l)(s) 
+
     sb += translate([0,0,spring_base_z + spring_h - spring_sink*2])(spring_top())
     lsw = translate([(spring_base_x - lswitch_x)/2.,(spring_base_y - lswitch_y)/2.,lswitch_sink - lswitch_z])( lswitch() )
-    tp = top_plate() + translate([0,0,top_to_roof_dist])(roof_plate()+color('red')(lsw))
-    sb += translate([0,0,spring_base_z * 4])(tp)
+    tp = top_plate() + translate([0,0,top_to_roof_dist])(roof_plate()+color('red')(lsw)) + translate([0,0,0])(color('red')(lsw))
+
+    top_plates_h = rod_h - top_to_roof_dist - rod_hole_start_h/2 + 1e-1
+    sb += translate([0,0,top_plates_h])(tp) 
 
     for i in spring_locs:
         sb += translate([0,0,spring_h - spring_sink])( translate(i)(color('orange')(bearing())))
-        sb += translate([0,0,spring_base_z * 4 - i[2]])( translate(i)(color('orange')(bearing())))
+        sb += translate([0,0,top_plates_h - i[2]])( translate(i)(color('orange')(bearing())))
         sb += translate([i[0], i[1], rod_hole_start_h])( color('pink')(rod()))
 
     for x in [0+pad, spring_base_x-pad]:
@@ -145,6 +172,26 @@ def assembly():
 
     lsw = translate([(spring_base_x - lswitch_x)/2.,(spring_base_y - lswitch_y)/2.,spring_base_z - lswitch_sink + 1e-3])( lswitch() )
     sb += color('red')(lsw)
+
+    sb += translate([0,pad*1.5/4,top_plates_h - spring_top_z])(color('orange')(sense_plate()))
+
+    mug = translate([spring_base_x/2, spring_base_y/2, spring_base_z + spring_h])( scale([0.18]*3)(import_stl("../mug.stl")) )
+
+    aero = cylinder(d = 11.0, h = 2.)
+    aero += translate([0,0,2.])(cylinder(d = 8.0, h = 10))
+    aero = color('Bisque')(aero)
+    aero2 = translate([0,0,12.])(cylinder(d = 7.0, h = 9.))
+    aero2 += translate([0,0,21.])(cylinder(d = 11.0, h = 2.))
+    aero2 = color('BurlyWood')(aero2)
+    aero += aero2
+    
+    aero = translate([spring_base_x/2, spring_base_y/2, spring_base_z + spring_h + 9])( aero )
+
+    sb += color('tan')(mug)
+    sb += (aero)
+
+    if SPLICE:
+        sb -= translate([-1e-3,-1e-3,0])(cube([100,pad,100]))
 
     return sb
 
